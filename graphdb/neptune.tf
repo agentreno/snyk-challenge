1a0717aeaeef9d4b4c6565e8618c7f363b3fb0d0
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-06 11:30:56 +0100

    Add neptune terraform module

---
+++ graphdb/neptune.tf
@@ -0,0 +1,29 @@
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = "${data.aws_vpc.default.id}"
+}
+
+resource "aws_neptune_cluster" "default" {
+    cluster_identifier = "snyk-challenge"
+    engine = "neptune"
+    skip_final_snapshot = true
+    apply_immediately = true
+    neptune_subnet_group_name = "${aws_neptune_subnet_group.default.name}"
+}
+
+resource "aws_neptune_cluster_instance" "default" {
+    count = 1
+    cluster_identifier = "${aws_neptune_cluster.default.id}"
+    instance_class = "db.r4.large"
+}
+
+resource "aws_neptune_subnet_group" "default" {
+    name = "default-group"
+    subnet_ids = [
+        "${element(data.aws_subnet_ids.default.ids, 0)}",
+        "${element(data.aws_subnet_ids.default.ids, 1)}"
+    ]
+}
