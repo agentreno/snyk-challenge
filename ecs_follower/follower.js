2c86aef456321ca4799404feace3a71134d13fd7
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-07 17:27:37 +0100

    Reduce logging to seq numbers and errors and start from seq0

--- ecs_follower/follower.js
+++ ecs_follower/follower.js
@@ -14,30 +14,30 @@ const g = new Graph().traversal().withRemote(dc)
 var changes = new ChangesStream({
    db: db,
    include_docs: true,
-   since: 'now',
 })
 
 changes.on('data', async function(change) {
+  console.log('Sequence: ' + change.seq + '\n')
   if (change.doc.name) {
     metadata = Normalize(change.doc)
 
     if (typeof metadata.versions === 'object') {
       package_name = change.doc.name
-      console.log('Processing update on ' + package_name)
+      //console.log('Processing update on ' + package_name)
 
       // Add package vertex if it doesn't exist
       package_vertex_resp = await g.V().has('package', 'name', package_name)
         .fold().coalesce(unfold(), addV('package').property('name', package_name)).next()
         .catch(err => { console.log('Error adding package vertex:\n' + err) })
-      console.log('Added package vertex:\n' + JSON.stringify(package_vertex_resp))
+      //console.log('Added package vertex:\n' + JSON.stringify(package_vertex_resp))
       var {value: {id: package_vertex_id}} = package_vertex_resp
 
       // Loop over package versions, adding dependencies to the graph
       Object.entries(metadata.versions).forEach(([version, version_metadata]) => {
         // Add version to the package version list
        g.V().has('package', 'name', package_name).property('versions', version).next()
-        .then(value => { console.log('Added package version:\n' + JSON.stringify(value)) })
         .catch(err => { console.log('Error adding package version:\n' + err) })
+        //.then(value => { console.log('Added package version:\n' + JSON.stringify(value)) })
 
         // Loop over dependencies for this version
         if (typeof version_metadata.dependencies === 'object') {
@@ -46,7 +46,7 @@ changes.on('data', async function(change) {
             dependency_vertex_resp = await g.V().has('package', 'name', dependency_name)
               .fold().coalesce(unfold(), addV('package').property('name', dependency_name)).next()
               .catch(err => { console.log('Error adding dependency vertex:\n' + err) })
-            console.log('Added dependency vertex:\n' + JSON.stringify(dependency_vertex_resp))
+            //console.log('Added dependency vertex:\n' + JSON.stringify(dependency_vertex_resp))
             var {value: {id: dependency_vertex_id}} = dependency_vertex_resp
 
             // Add depends edge from package to dependency with constraint as property
@@ -55,8 +55,8 @@ changes.on('data', async function(change) {
               .property('from_version', version)
               .property('to_version', version_constraint)
               .next()
-              .then(value => { console.log('Added dependency edge:\n' + JSON.stringify(value)) })
               .catch(err => { console.log('Error adding dependency edge:\n' + err) })
+              //.then(value => { console.log('Added dependency edge:\n' + JSON.stringify(value)) })
           })
         }
       })
