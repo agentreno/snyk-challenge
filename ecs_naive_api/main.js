5f7e183ac09ee80cf81a8ac8f3bf274935705242
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-13 16:32:34 +0100

    Add cache lookup and setex behaviour to naive API

--- ecs_naive_api/main.js
+++ ecs_naive_api/main.js
@@ -1,14 +1,40 @@
 const express = require('express')
+const redis = require('redis')
+const {promisify} = require('util')
 const ls = require('npm-remote-ls').ls
 const app = express()
 const port = 8000
+
+client = redis.createClient({ 'host': process.env.CACHE_CONNECTION_STRING })
+const getAsync = promisify(client.get).bind(client)
+const setEx = promisify(client.set).bind(client)
 
 app.get('/health', (req, res) => {
     res.send('OK')
 })
 
-app.get('/package/:package/:version', (req, res) => {
+app.get('/package/:package/:version', async function(req, res) {
+  const cache_key = 'deps:' + req.params['package'] + '@' + req.params['version']
+
+  try {
+    cached_tree = await getAsync(cache_key)
+    if (cached_tree) {
+      res.send(JSON.parse(cached_tree))
+      return
+    }
+  } catch(err) {
+    console.log('Cache get miss / fail')
+    console.log(err)
+  }
+
   ls(req.params['package'], req.params['version'], tree => {
+    try {
+      client.set(cache_key, JSON.stringify(tree), 'EX', 3600)
+    } catch(err) {
+      console.log('Cache setex miss / fail')
+      console.log(err)
+    }
+
     res.send(tree)
   })
 })
