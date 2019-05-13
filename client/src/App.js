5984656194a84ac2e7ace5eee1b9878f341cce98
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-13 17:40:04 +0100

    Fetch data from naive API in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Tree from 'react-d3-tree';
+import axios from 'axios';
 import './App.css';
 
-const myTreeData = [
+const base_api = 'http://snyk-challenge-naive-api-1433142573.eu-west-1.elb.amazonaws.com'
+
+var myTreeData = [
   {
     name: 'Top Level',
     attributes: {
@@ -29,24 +32,22 @@ const myTreeData = [
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { package: '' }
+    this.state = { package: '', treeData: [{}] }
     this.handlePackageChange = this.handlePackageChange.bind(this)
   }
 
   handlePackageChange(e) {
     this.setState({ package: e.target.value })
+    axios.get(base_api + '/package/' + e.target.value + '/latest/').then(resp => {
+      this.setState({ treeData: [resp.data] })
+    }).catch(err => { console.log(err) })
   }
 
   render() {
-    let treeData = [{}]
-    if (this.state.package === 'package') {
-      treeData = myTreeData
-    }
-
     return (
       <div className="App">
         <div id="treeWrapper">
-          <Tree data={treeData} translate={{x: 50, y: 400}} />
+          <Tree data={this.state.treeData} translate={{x: 50, y: 400}} />
           <div id="packageInput">
             <p>Insert the name of a package here:</p>
             <input value={this.state.package} onChange={this.handlePackageChange} />
