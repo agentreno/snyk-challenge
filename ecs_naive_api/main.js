17c11bf823906be200e5e0421b90d2a2a4cbde08
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-13 13:04:56 +0100

    Add package endpoint using npm-remote-ls

--- ecs_naive_api/main.js
+++ ecs_naive_api/main.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const ls = require('npm-remote-ls').ls
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/package/:package/:version', (req, res) => {
+  ls(req.params['package'], req.params['version'], tree => {
+    res.send(tree)
+  })
+})
 
 app.listen(port, () => console.log('Listening...'))
