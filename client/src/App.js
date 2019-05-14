f3327e6248262af2d0d27959ccc68dabbd5ac6ae
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-14 19:25:04 +0100

    Improve UI look and feel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tree from 'react-d3-tree';
-import {debounce} from 'lodash'
+import {debounce} from 'lodash';
+import {BounceLoader} from 'react-spinners';
 import axios from 'axios';
 import './App.css';
 
@@ -11,21 +12,30 @@ function convertRespToGraph(data) {
     return Object.entries(data).map(([k, v]) => ({'name': k, 'children': convertRespToGraph(v)}))
 }
 
+function countObjKeys(obj) {
+  if (typeof obj !== 'object' || obj === null) return 0
+  const keys = Object.keys(obj);
+  let sum = keys.length;
+  keys.forEach(key => sum += countObjKeys(obj[key]));
+  return sum;
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = { package: 'react', version: 'latest', treeData: [{}] }
     this.handlePackageChange = this.handlePackageChange.bind(this)
     this.handleVersionChange = this.handleVersionChange.bind(this)
-    this.fetchPackageDependencies = debounce(this.fetchPackageDependencies, 250)
+    this.fetchPackageDependencies = debounce(this.fetchPackageDependencies, 750)
   }
 
   fetchPackageDependencies(packageName, versionName) {
     versionName = versionName || 'latest'
+    this.setState({ loading: true })
     axios.get(`${base_api}/package/${packageName}/${versionName}/`).then(resp => {
       let data = convertRespToGraph(resp.data)
       if (Object.keys(data).length === 0) data = [{}]
-      this.setState({ treeData: data })
+      this.setState({ treeData: data, loading: false })
     }).catch(err => { console.log(err) })
   }
 
@@ -47,13 +57,32 @@ class App extends Component {
     return (
       <div className="App">
         <div id="treeWrapper">
-          <Tree data={this.state.treeData} translate={{x: 50, y: 400}} />
+          <Tree
+            data={this.state.treeData}
+            orientation="vertical"
+            translate={{ x: 800, y: 30 }}
+            textLayout={{ textAnchor: "start", x: -30, y: 30 }}
+            className={this.state.loading ? 'loading' : null}
+            transitionDuration={countObjKeys(this.state.treeData) > 50 ? 0 : 500}
+          />
           <div id="packageInput">
-            <p>Insert the name of a package here:</p>
-            <input value={this.state.package} onChange={this.handlePackageChange} />
-            <p>Insert the version of a package here:</p>
-            <input value={this.state.version} onChange={this.handleVersionChange} />
+            <fieldset>
+              <legend>Package Name</legend>
+              <input value={this.state.package} onChange={this.handlePackageChange} />
+            </fieldset>
+            <fieldset>
+              <legend>Package Version</legend>
+              <input value={this.state.version} onChange={this.handleVersionChange} />
+            </fieldset>
           </div>
+        </div>
+        <div id="loader">
+          <BounceLoader
+            sizeUnit={"px"}
+            size={60}
+            color={"#66E0C8"}
+            loading={this.state.loading}
+          />
         </div>
       </div>
     );
