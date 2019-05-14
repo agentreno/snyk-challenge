cd945dcee1a65c40b6019e61c4b732f6eddb0c69
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-14 13:36:55 +0100

    Comment out graph/follower resources

--- root.tf
+++ root.tf
@@ -1,16 +1,16 @@
-module "graphdb" {
-    source = "./graphdb"
-}
+#module "graphdb" {
+#    source = "./graphdb"
+#}
 
-module "ecs_follower" {
-    source = "./ecs_follower"
-    database_endpoint = "${module.graphdb.database_endpoint}"
-}
+#module "ecs_follower" {
+#    source = "./ecs_follower"
+#    database_endpoint = "${module.graphdb.database_endpoint}"
+#}
 
-module "ecs_api" {
-    source = "./ecs_api"
-    database_endpoint = "${module.graphdb.database_endpoint}"
-}
+#module "ecs_api" {
+#    source = "./ecs_api"
+#    database_endpoint = "${module.graphdb.database_endpoint}"
+#}
 
 module "ecs_naive_api" {
     source = "./ecs_naive_api"
@@ -21,9 +21,9 @@ module "cache" {
     source = "./cache"
 }
 
-output "api_url" {
-    value = "http://${module.ecs_api.elb_dns}/api/package/<package name>/<version>/"
-}
+#output "api_url" {
+#    value = "http://${module.ecs_api.elb_dns}/api/package/<package name>/<version>/"
+#}
 
 output "naive_api_url" {
     value = "http://${module.ecs_naive_api.elb_dns}/package/<package name>/<version>/"
